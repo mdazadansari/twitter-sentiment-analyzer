2922fa1ee9f1dfeea759225abd2627c136e55a9f
mdazadansari <786azadmd@gmail.com>
2021-12-31 07:54:20 +0530

    added by git desktop

---
+++ app.py
@@ -0,0 +1,119 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from streamlit.elements import layouts
+import preprocessing
+import tweepy
+from tweepy import OAuthHandler
+import pickle5
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+st.set_page_config(page_title="Twitter Sentiment Analyzer", layout="wide", )
+st.title("Welcome To Twitter Sentiment Analyzer")
+st.text("Analysis is done by Logistic Regression Classifier which gave accuracy = 80.8975%")
+st.sidebar.title("Twitter Sentiment Analyzer")
+with st.sidebar:
+    with st.form(key="form1"):
+        #consumerKey =  st.text_input('Enter Consumer_Key')
+        #consumer_secret =  st.text_input('Enter Consumer_Secret')
+        #accessToken =  st.text_input('Enter Access_Token')
+        #accessTokenSecret =  st.text_input('Enter Access_Token_Secret')
+        keyword = st.text_input("Enter Keyword to Search")
+        number_of_tweet = st.number_input("Enter Number of Tweets", min_value=1, step=1)
+        submitted1 = st.form_submit_button('Submit')
+        
+def Api_Handling(consumer_key, consumer_secret, access_token, acces_token_secret):
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, acces_token_secret)
+    api = tweepy.API(auth)
+    return api
+
+def get_tweets(api, query, count):
+    tweets = []
+    fetched_tweets = api.search_tweets(q=query, count=count, lang='en')
+    # parsing tweets one by one
+    for tweet in fetched_tweets:
+        parsed_tweet = {}
+        # saving text of tweet
+        parsed_tweet['tweet'] = tweet.text
+
+        # appending parsed tweet to tweets list
+        if tweet.retweet_count > 0:
+            # if tweet has retweets, ensure that it is appended only once
+            if parsed_tweet not in tweets:
+                tweets.append(parsed_tweet)
+        else:
+            tweets.append(parsed_tweet)
+    # return parsed tweets
+    return tweets
+
+ 
+consumerKey = st.secrets["consumer_Key"]
+consumerSecret = st.secrets["consumer_Secret"]
+accessToken = st.secrets["access_Token"]
+accessTokenSecret = st.secrets["access_Token_Secret"]
+
+
+
+api = Api_Handling(consumerKey, consumerSecret, accessToken, accessTokenSecret)
+if submitted1:
+    tweets = get_tweets(api, keyword, number_of_tweet)
+    tweets_df = pd.DataFrame(tweets)
+    st.header("Downloaded Tweets")
+    st.write(tweets_df)
+
+    tweets_df['tweet'] = np.vectorize(preprocessing.process_tweet)(tweets_df['tweet'])
+
+    tweets_df['tweet'] = preprocessing.lematize(tweets_df)
+
+
+    load_vectoriser = pd.read_pickle(r"vectoriser")
+    tweets_df_vector = load_vectoriser.transform(tweets_df['tweet'])
+
+    load_model = pd.read_pickle(r"classifier")
+    predicted = load_model.predict(tweets_df_vector)
+
+    tweets_df['sentiment'] = predicted
+    tweets_df.loc[tweets_df['sentiment'] == 0, 'sentiment'] = 'negative'
+    tweets_df.loc[tweets_df['sentiment'] == 1, 'sentiment'] = 'positive'
+
+    st.header("Tweets with sentiment")
+    st.write(tweets_df)
+
+    st.header("Wordcloud of tweets")
+    #plt.figure(figsize = (20,20))
+    wc = WordCloud(width = 1600 , height = 800,
+               collocations=False).generate(" ".join(tweets_df['tweet']))
+    fig = plt.figure()
+    plt.imshow(wc)
+    plt.axis("off")
+    st.pyplot(fig)
+
+    positive_tweets = tweets_df[tweets_df['sentiment']=='positive']['tweet']
+    negative_tweets = tweets_df[tweets_df['sentiment']=='negative']['tweet']
+
+    st.header("Wordcloud of positive tweets")
+    wc = WordCloud(width = 1600 , height = 800,
+               collocations=False).generate(" ".join(positive_tweets))
+    fig = plt.figure()
+    plt.imshow(wc)
+    plt.axis("off")
+    st.pyplot(fig)
+
+    st.header("Wordcloud of negative tweets")
+    wc = WordCloud(width = 1600 , height = 800,
+               collocations=False).generate(" ".join(negative_tweets))
+    fig = plt.figure()
+    plt.imshow(wc)
+    plt.axis("off")
+    st.pyplot(fig)
+
+
+    st.header("Countplot of Tweets")
+    fig = plt.figure()
+    sns.countplot(x ='sentiment', data = tweets_df, palette ='coolwarm')
+    st.pyplot(fig)
+
---
+++ preprocessing.py
@@ -0,0 +1,87 @@
+# utilities
+import re
+import numpy as np
+import pandas as pd
+import json
+# plotting
+import seaborn as sns
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+# nltk
+import nltk
+from nltk.stem import WordNetLemmatizer
+# sklearn
+from sklearn.svm import LinearSVC
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
+
+#nltk.download('wordnet')
+
+stop_words = {"i", "me", "my", "myself", "we", "our", "ours", "ourselves",
+             "you", "your", "yours", "yourself", "yourselves", "he", "him",
+             "his", "himself", "she", "her", "hers", "herself", "it", "its",
+             "itself", "they", "them", "their", "theirs", "themselves", "what",
+             "which", "who", "whom", "this", "that", "these", "those", "am", "is",
+             "are", "was", "were", "be", "been", "being", "have", "has", "had",
+             "having", "do", "does", "did", "doing", "a", "an", "the", "and",
+             "but", "if", "or", "because", "as", "until", "while", "of", "at",
+             "by", "for", "with", "about", "against", "between", "into", "through",
+             "during", "before", "after", "above", "below", "to", "from", "up",
+             "down", "in", "out", "on", "off", "over", "under", "again", "further",
+             "then", "once", "here", "there", "when", "where", "why", "how", "all",
+             "any", "both", "each", "few", "more", "most", "other", "some", "such",
+             "only", "own", "same", "so", "than", "too", "very",
+             "can", "will", "just", "should", "now", "rt"}
+
+def emoji(tweet):
+    # Smile -- :), : ), :-), (:, ( :, (-:, :') , :O
+    tweet = re.sub(r'(:\s?\)|:-\)|\(\s?:|\(-:|:\'\)|:O)', ' positiveemoji ', tweet)
+    # Laugh -- :D, : D, :-D, xD, x-D, XD, X-D
+    tweet = re.sub(r'(:\s?D|:-D|x-?D|X-?D)', ' positiveemoji ', tweet)
+    # Love -- <3, :*
+    tweet = re.sub(r'(<3|:\*)', ' positiveemoji ', tweet)
+    # Wink -- ;-), ;), ;-D, ;D, (;,  (-; , @-)
+    tweet = re.sub(r'(;-?\)|;-?D|\(-?;|@-\))', ' positiveemoji ', tweet)
+    # Sad -- :-(, : (, :(, ):, )-:, :-/ , :-|
+    tweet = re.sub(r'(:\s?\(|:-\(|\)\s?:|\)-:|:-/|:-\|)', ' negetiveemoji ', tweet)
+    # Cry -- :,(, :'(, :"(
+    tweet = re.sub(r'(:,\(|:\'\(|:"\()', ' negetiveemoji ', tweet)
+    return tweet
+
+with open('contractions.json', 'r') as f:
+    contractions_dict = json.load(f)
+contractions = contractions_dict['contractions']
+
+def process_tweet(tweet):
+    tweet = tweet.lower()                                             # Lowercases the string
+    tweet = re.sub('@[^\s]+', '', tweet)                              # Removes usernames
+    tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', ' ', tweet)   # Remove URLs
+    tweet = re.sub(r"\d+", " ", str(tweet))                           # Removes all digits
+    tweet = re.sub('&quot;'," ", tweet)                               # Remove (&quot;) 
+    tweet = emoji(tweet)                                              # Replaces Emojis
+    tweet = re.sub(r"\b[a-zA-Z]\b", "", str(tweet))                   # Removes all single characters
+    for word in tweet.split():
+        if word.lower() in contractions:
+            tweet = tweet.replace(word, contractions[word.lower()])   # Replaces contractions
+    tweet = re.sub(r"[^\w\s]", " ", str(tweet))                       # Removes all punctuations
+    tweet = re.sub(r'(.)\1+', r'\1\1', tweet)                         # Convert more than 2 letter repetitions to 2 letter
+    tweet = re.sub(r"\s+", " ", str(tweet))                           # Replaces double spaces with single space    
+    return tweet
+
+def cleaning_stopwords(tweet):
+    return " ".join(word for word in tweet if word not in stop_words)
+
+def lematize(tweets_df):
+    #text splitting
+    tweets_df['tweet'] = tweets_df['tweet'].apply(lambda x: x.split())
+
+    lemmatizer_tweet = WordNetLemmatizer()
+    tweets_df['tweet'] = tweets_df['tweet'].apply(lambda x: [lemmatizer_tweet.lemmatize(i) for i in x])
+
+    tweets_df['tweet'] = tweets_df['tweet'].apply(lambda tweet: cleaning_stopwords(tweet))
+    
+    return tweets_df['tweet']
+
